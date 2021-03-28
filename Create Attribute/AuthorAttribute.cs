d6e66a780c0b262e7ae10f70428409ae9d813a5c
Kiril Kirilov <kirilkirilov@abv.bg>
2021-03-28 23:51:36 +0300

    Add files via upload

---
+++ Create Attribute/AuthorAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Create_Attribute
+{
+    class AuthorAttribute
+    {
+    }
+}
---
+++ Create Attribute/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Create_Attribute
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
