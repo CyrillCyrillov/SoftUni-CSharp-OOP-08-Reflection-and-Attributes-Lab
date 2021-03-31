25e68fa7ae4220ce5a6088c5e08f9c20e09868e1
Kiril Kirilov <kirilkirilov@abv.bg>
2021-03-31 23:24:04 +0300

    Add files via upload

--- Create Attribute/AuthorAttribute.cs
+++ Create Attribute/AuthorAttribute.cs
@@ -1,10 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
-namespace Create_Attribute
+namespace AuthorProblem
 {
-    class AuthorAttribute
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true)]
+    
+    public class AuthorAttribute : Attribute
     {
+
+        public AuthorAttribute(string name)
+        {
+            this.Name = name;
+        }
+
+        public string Name { get; set; }
     }
+
 }
+
